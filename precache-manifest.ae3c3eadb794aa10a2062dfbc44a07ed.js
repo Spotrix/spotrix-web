15247e78ca30fa5be390fb54448acd1f3233dfd4
ciusji <bqjimaster@gmail.com>
2021-05-30 12:32:45 +0800

    Updates

---
+++ precache-manifest.ae3c3eadb794aa10a2062dfbc44a07ed.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba93cb4d1755a45b1ba72cb4ddab7e3c",
+    "url": "/spotrix-web/index.html"
+  },
+  {
+    "revision": "e2988a99227bdb8f4775",
+    "url": "/spotrix-web/static/js/2.22f72dcd.chunk.js"
+  },
+  {
+    "revision": "95bf5aa663b506694308",
+    "url": "/spotrix-web/static/js/main.6c342b98.chunk.js"
+  },
+  {
+    "revision": "f80ebc8faf9402d2b1bc",
+    "url": "/spotrix-web/static/js/runtime~main.2cee48ba.js"
+  },
+  {
+    "revision": "03af92f39c4012ec17c92438d2caee82",
+    "url": "/spotrix-web/static/media/api.03af92f3.svg"
+  },
+  {
+    "revision": "93f63f6d7d8b2e2fe6319873845ef864",
+    "url": "/spotrix-web/static/media/app.93f63f6d.svg"
+  },
+  {
+    "revision": "5ba8b32fbaabcdb0ef5fc72b5f0a9801",
+    "url": "/spotrix-web/static/media/bg-main.5ba8b32f.svg"
+  },
+  {
+    "revision": "d1ffc25a19fdbfaa837f9a857dc23ce4",
+    "url": "/spotrix-web/static/media/bg-secondary.d1ffc25a.svg"
+  },
+  {
+    "revision": "dbdbb6bee63c35bfd175f30b94d79035",
+    "url": "/spotrix-web/static/media/bi.dbdbb6be.svg"
+  },
+  {
+    "revision": "f3b0550e8a98be1ba2bb3a8d4f93624d",
+    "url": "/spotrix-web/static/media/comma.f3b0550e.svg"
+  },
+  {
+    "revision": "606a1266652f310996dca76db9fe52d3",
+    "url": "/spotrix-web/static/media/custom.606a1266.svg"
+  },
+  {
+    "revision": "9e8f59ec9b5b45a82c8dfcd060f05e2e",
+    "url": "/spotrix-web/static/media/dashboard.9e8f59ec.svg"
+  },
+  {
+    "revision": "6e47119ccb65ad077da83f9b9ceb7992",
+    "url": "/spotrix-web/static/media/dataflow.6e47119c.svg"
+  },
+  {
+    "revision": "89f8030495db73a02cd1c30135a5f7f1",
+    "url": "/spotrix-web/static/media/ff_1.89f80304.svg"
+  },
+  {
+    "revision": "e6c06c1c2af1a68857d72514285bc984",
+    "url": "/spotrix-web/static/media/ff_2.e6c06c1c.svg"
+  },
+  {
+    "revision": "21b09d7c31f2dadc1a322c39fdd6e805",
+    "url": "/spotrix-web/static/media/ff_3.21b09d7c.svg"
+  },
+  {
+    "revision": "c50ae4c1a822bcdb61e8b26e1d7f9a6b",
+    "url": "/spotrix-web/static/media/ff_4.c50ae4c1.svg"
+  },
+  {
+    "revision": "2d7ee4e2eefe9058af6e401be84e8013",
+    "url": "/spotrix-web/static/media/github.2d7ee4e2.svg"
+  },
+  {
+    "revision": "0f59ea5cfd88fa71e1f22386df088767",
+    "url": "/spotrix-web/static/media/glab.0f59ea5c.png"
+  },
+  {
+    "revision": "6123b4638303e2178fd15263ffb3b2a8",
+    "url": "/spotrix-web/static/media/insights.6123b463.svg"
+  },
+  {
+    "revision": "b5ed5d9d3430e61f2626c40b76393116",
+    "url": "/spotrix-web/static/media/linkedin.b5ed5d9d.svg"
+  },
+  {
+    "revision": "4f2fffa8217f01ff6d7e702b6d11f402",
+    "url": "/spotrix-web/static/media/model.4f2fffa8.svg"
+  },
+  {
+    "revision": "0d3667d3bb0e9ea2f569351b7b7188c1",
+    "url": "/spotrix-web/static/media/monitor.0d3667d3.svg"
+  },
+  {
+    "revision": "5e78f35ffd1003a95a6ee1e8515511ed",
+    "url": "/spotrix-web/static/media/platform.5e78f35f.svg"
+  },
+  {
+    "revision": "84975c076edd098b63435cef5829fb1a",
+    "url": "/spotrix-web/static/media/pre.84975c07.svg"
+  },
+  {
+    "revision": "cbc69166975e20e94c0eeeaad566b996",
+    "url": "/spotrix-web/static/media/security.cbc69166.svg"
+  },
+  {
+    "revision": "62933c751ba228370e5c04c782f258dd",
+    "url": "/spotrix-web/static/media/spotrix-footer.62933c75.svg"
+  },
+  {
+    "revision": "6d45175ad879537cdd38b1f4500454bc",
+    "url": "/spotrix-web/static/media/spotrix.6d45175a.svg"
+  }
+]);
