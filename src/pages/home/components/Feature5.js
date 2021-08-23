3c7980bc43f8ebe1366817bfc193e67025450247
ciusji <bqjimaster@gmail.com>
2021-08-23 17:56:40 +0800

    update feature slogan

--- src/pages/home/components/Feature5.js
+++ src/pages/home/components/Feature5.js
@@ -59,13 +59,13 @@ export default function Feature5() {
             <Grid container>
               <Grid item xs={9}>
                 <img src={CommaIcon} width={20} alt="comma"/>
-                <p>Spotrix helps users sketch and share professional data-flow processing, providing business intelligence tools for
+                <p><i>Spotrix helps users sketch and share professional data-flow processing, providing business intelligence tools for
                   everyone who want to data management. I don’t think enough people know how helpful
                   this technology can be, but just try it and you will do better.
+                </i></p>
+                <p className={classes.smallFont}>
+                  - Ciusji, administrator of GuinsooLab
                 </p>
-                <i className={classes.smallFont}>
-                  - ciusji, administrator of GuinsooLab
-                </i>
               </Grid>
               <Grid item xs={3} className={classes.avatarGrid}>
                 <Avatar
