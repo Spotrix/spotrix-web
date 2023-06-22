67504df6322ec21b001fc6f6540129aea0a54c79
ciusji <bqjimaster@gmail.com>
2023-06-22 10:10:03 +0800

    Updates

---
+++ precache-manifest.db9db0c478c1e8d2c0ef5d2589cca613.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3efd2985c4c4f4382057a782eed613e4",
+    "url": "/spotrix-web/index.html"
+  },
+  {
+    "revision": "1cdda0d41041b6d8e63b",
+    "url": "/spotrix-web/static/js/2.dff490ca.chunk.js"
+  },
+  {
+    "revision": "f9e872b4a613ab780306",
+    "url": "/spotrix-web/static/js/main.559c215d.chunk.js"
+  },
+  {
+    "revision": "f80ebc8faf9402d2b1bc",
+    "url": "/spotrix-web/static/js/runtime~main.2cee48ba.js"
+  },
+  {
+    "revision": "b1b16fe556e14a1d1cb53e233e07d266",
+    "url": "/spotrix-web/static/media/augment-diagnose.b1b16fe5.png"
+  },
+  {
+    "revision": "79dcbe3e03482f54d40e7bd7ac8fcf0a",
+    "url": "/spotrix-web/static/media/kpi-mrr-hero.79dcbe3e.png"
+  },
+  {
+    "revision": "1e8aac5f0ff2b49eb0206636b57d6b7a",
+    "url": "/spotrix-web/static/media/monitor-alert.1e8aac5f.png"
+  },
+  {
+    "revision": "bf19c552e7fe7e8d9da197d5ef837e0f",
+    "url": "/spotrix-web/static/media/predict-act.bf19c552.png"
+  },
+  {
+    "revision": "66c9febb890c3604b0772d5c67463531",
+    "url": "/spotrix-web/static/media/right-image-v2.66c9febb.png"
+  },
+  {
+    "revision": "5fb1d511ee877000f28a7adb246fbe6d",
+    "url": "/spotrix-web/static/media/spotrix-footer.5fb1d511.svg"
+  },
+  {
+    "revision": "977c34c9b8ab597358aeef01bb81168b",
+    "url": "/spotrix-web/static/media/spotrix.977c34c9.svg"
+  }
+]);
